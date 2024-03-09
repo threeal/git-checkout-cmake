bec295e0fcfe95bde9e30278939ba3ada226887d
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-03-09 22:45:45 +0700

    test: add test for `_set_error` function

--- test/GitCheckoutTest.cmake
+++ test/GitCheckoutTest.cmake
@@ -5,6 +5,48 @@ endif()
 
 set(TEST_COUNT 0)
 
+if("Set error" MATCHES ${TEST_MATCHES})
+  math(EXPR TEST_COUNT "${TEST_COUNT} + 1")
+
+  set(MOCK_MESSAGE on)
+  macro(message MODE MESSAGE)
+    if(MOCK_MESSAGE)
+      set(${MODE}_MESSAGE "${MESSAGE}" PARENT_SCOPE)
+    else()
+      _message(${MODE} ${MESSAGE})
+    endif()
+  endmacro()
+
+  include(GitCheckout)
+
+  function(foo)
+    _set_error("Unknown error")
+  endfunction()
+
+  foo()
+
+  set(MOCK_MESSAGE off)
+  if(NOT FATAL_ERROR_MESSAGE STREQUAL "Unknown error")
+    message(FATAL_ERROR "It should have set the error to 'Unknown error' but instead got '${FATAL_ERROR_MESSAGE}'")
+  endif()
+endif()
+
+if("Set error with variable specified" MATCHES ${TEST_MATCHES})
+  math(EXPR TEST_COUNT "${TEST_COUNT} + 1")
+
+  include(GitCheckout)
+
+  function(foo)
+    _set_error("Unknown error" ERROR_VARIABLE ERR)
+  endfunction()
+
+  foo()
+
+  if(NOT ERR STREQUAL "Unknown error")
+    message(FATAL_ERROR "It should have set the error to 'Unknown error' but instead got '${ERR}'")
+  endif()
+endif()
+
 if("Check out a Git repository" MATCHES ${TEST_MATCHES})
   math(EXPR TEST_COUNT "${TEST_COUNT} + 1")
 
