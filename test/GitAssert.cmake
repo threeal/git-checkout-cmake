b57e4a5bdb95f67138298b746c3321b0c058e283
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-03-11 21:32:43 +0700

    test: check if Git checked out completely in testing (#19)
    * test: add `assert_git_complete_checkout` function
    
    * test: check for Git diff in `assert_git_complete_checkout` function

---
+++ test/GitAssert.cmake
@@ -0,0 +1,17 @@
+# Asserts whether a Git repository is checked out completely.
+#
+# Arguments:
+#   - DIRECTORY: The path of the directory to check out the Git repository.
+function(assert_git_complete_checkout DIRECTORY)
+  if(NOT EXISTS ${DIRECTORY})
+    message(FATAL_ERROR "the '${DIRECTORY}' directory should exist")
+  endif()
+
+  execute_process(
+    COMMAND git -C ${DIRECTORY} diff --no-patch --exit-code
+    RESULT_VARIABLE RES
+  )
+  if(NOT RES EQUAL 0)
+    message(FATAL_ERROR "the repository should be checked out completely (${RES})")
+  endif()
+endfunction()
