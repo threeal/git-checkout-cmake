4469558223b02559364fa29b290ddb5e57001b80
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-03-11 15:15:13 +0700

    feat: add support to check out on a specific reference (#12)
    * feat: add `REF` argument in `git_checkout` function
    
    * test: add test for checking out Git repo on a specific ref
    
    * feat: use `git checkout` instead to check out specific reference

--- cmake/GitCheckout.cmake
+++ cmake/GitCheckout.cmake
@@ -24,16 +24,38 @@ endmacro()
 #
 # Arguments:
 #   - URL: The URL of the remote Git repository.
+#
+# Optional arguments:
+#   - REF: The reference (branch, tag, or commit) to check out the Git repository.
 function(git_checkout URL)
-  cmake_parse_arguments(ARG "" "ERROR_VARIABLE" "" ${ARGN})
+  cmake_parse_arguments(ARG "" "REF;ERROR_VARIABLE" "" ${ARGN})
+
+  # Clones the Git repository.
   execute_process(
     COMMAND git clone ${URL}
     RESULT_VARIABLE RES
   )
   if(NOT RES EQUAL 0)
     _set_error(
-      "Failed to clone ${URL} (${RES})"
+      "Failed to clone '${URL}' (${RES})"
       ERROR_VARIABLE ${ARG_ERROR_VARIABLE}
     )
+  endif()
+
+  # Determines the directory of the cloned Git repository.
+  string(REGEX REPLACE ".*/" "" GIT_DIR ${URL})
+
+  if(ARG_REF)
+    # Checks out the Git repository to a specific reference.
+    execute_process(
+      COMMAND git -C ${GIT_DIR} checkout ${ARG_REF}
+      RESULT_VARIABLE RES
+    )
+    if(NOT RES EQUAL 0)
+      _set_error(
+        "Failed to check out '${GIT_DIR}' to '${ARG_REF}' (${RES})"
+        ERROR_VARIABLE ${ARG_ERROR_VARIABLE}
+      )
+    endif()
   endif()
 endfunction()
