1d97d4ee67d66232ce77880613831b8204555735
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-03-11 23:31:41 +0700

    test: add test for checking out Git repo sparsely

--- test/GitAssert.cmake
+++ test/GitAssert.cmake
@@ -1,3 +1,5 @@
+cmake_minimum_required(VERSION 3.3)
+
 # Asserts whether a Git repository is checked out completely.
 #
 # Arguments:
@@ -14,4 +16,25 @@ function(assert_git_complete_checkout DIRECTORY)
   if(NOT RES EQUAL 0)
     message(FATAL_ERROR "the repository should be checked out completely (${RES})")
   endif()
+endfunction()
+
+# Asserts whether a Git repository is checked out sparsely.
+#
+# Arguments:
+#   - DIRECTORY: The path of the directory to check out the Git repository.
+#
+# Optional arguments:
+#   - FILES: A list of files that should be checked out sparsely.
+function(assert_git_sparse_checkout DIRECTORY)
+  cmake_parse_arguments(ARG "" "" "FILES" ${ARGN})
+
+  assert_git_complete_checkout(${DIRECTORY})
+
+  file(GLOB FILES RELATIVE ${DIRECTORY} "${DIRECTORY}/**/*")
+  list(FILTER FILES EXCLUDE REGEX ${DIRECTORY}/.git)
+  foreach(FILE ${FILES})
+    if(NOT FILE IN_LIST ARG_FILES)
+      message(FATAL_ERROR "the '${FILE}' should not exist")
+    endif()
+  endforeach()
 endfunction()
