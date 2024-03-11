594b63a7749dd410ed740a9da0a099eb55566319
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-03-11 21:30:28 +0700

    test: check for Git diff in `assert_git_complete_checkout` function

--- test/GitAssert.cmake
+++ test/GitAssert.cmake
@@ -6,4 +6,12 @@ function(assert_git_complete_checkout DIRECTORY)
   if(NOT EXISTS ${DIRECTORY})
     message(FATAL_ERROR "the '${DIRECTORY}' directory should exist")
   endif()
+
+  execute_process(
+    COMMAND git -C ${DIRECTORY} diff --no-patch --exit-code
+    RESULT_VARIABLE RES
+  )
+  if(NOT RES EQUAL 0)
+    message(FATAL_ERROR "the repository should be checked out completely (${RES})")
+  endif()
 endfunction()
