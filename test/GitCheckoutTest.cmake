689cab5e8750acedf5428fe2f2c771a3dae2de64
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-03-11 14:11:18 +0700

    feat: add error handling function (#8)
    * feat: add `_set_error` function
    
    * test: add test for `_set_error` function
    
    * test: merge test for checking out invalid Git repo

--- test/GitCheckoutTest.cmake
+++ test/GitCheckoutTest.cmake
@@ -5,6 +5,48 @@ endif()
 
 set(TEST_COUNT 0)
 
+if("Set error" MATCHES ${TEST_MATCHES})
+  math(EXPR TEST_COUNT "${TEST_COUNT} + 1")
+
+  set(MOCK_MESSAGE on)
+  macro(message MODE MESSAGE)
+    if(MOCK_MESSAGE)
+      set(${MODE}_MESSAGE "${MESSAGE}" PARENT_SCOPE)
+    else()
+      _message(${MODE} ${MESSAGE})
+    endif()
+  endmacro()
+
+  include(GitCheckout)
+
+  function(foo)
+    _set_error("Unknown error")
+  endfunction()
+
+  foo()
+
+  set(MOCK_MESSAGE off)
+  if(NOT FATAL_ERROR_MESSAGE STREQUAL "Unknown error")
+    message(FATAL_ERROR "It should have set the error to 'Unknown error' but instead got '${FATAL_ERROR_MESSAGE}'")
+  endif()
+endif()
+
+if("Set error with variable specified" MATCHES ${TEST_MATCHES})
+  math(EXPR TEST_COUNT "${TEST_COUNT} + 1")
+
+  include(GitCheckout)
+
+  function(foo)
+    _set_error("Unknown error" ERROR_VARIABLE ERR)
+  endfunction()
+
+  foo()
+
+  if(NOT ERR STREQUAL "Unknown error")
+    message(FATAL_ERROR "It should have set the error to 'Unknown error' but instead got '${ERR}'")
+  endif()
+endif()
+
 if("Check out a Git repository" MATCHES ${TEST_MATCHES})
   math(EXPR TEST_COUNT "${TEST_COUNT} + 1")
 
@@ -16,36 +58,11 @@ if("Check out a Git repository" MATCHES ${TEST_MATCHES})
   git_checkout(https://github.com/threeal/project-starter)
 
   if(NOT EXISTS project-starter)
-    message(FATAL_ERROR "project-starter should exist")
+    message(FATAL_ERROR "The 'project-starter' directory should exist")
   endif()
 endif()
 
 if("Check out an invalid Git repository" MATCHES ${TEST_MATCHES})
-  math(EXPR TEST_COUNT "${TEST_COUNT} + 1")
-
-  set(MOCK_MESSAGE on)
-  macro(message MODE MESSAGE)
-    if(MOCK_MESSAGE)
-      if(${MODE} STREQUAL FATAL_ERROR)
-        set(FATAL_ERROR_MESSAGE "${MESSAGE}" PARENT_SCOPE)
-      endif()
-    else()
-      _message(${MODE} ${MESSAGE})
-    endif()
-  endmacro()
-
-  include(GitCheckout)
-  git_checkout(https://github.com/threeal/invalid-project)
-
-  set(MOCK_MESSAGE off)
-
-  set(EXPECTED_FATAL_ERROR_MESSAGE "Failed to clone https://github.com/threeal/invalid-project (128)")
-  if(NOT FATAL_ERROR_MESSAGE STREQUAL EXPECTED_FATAL_ERROR_MESSAGE)
-    message(FATAL_ERROR "It should fail to check out because of `${EXPECTED_FATAL_ERROR_MESSAGE}`, but got `${FATAL_ERROR_MESSAGE}` instead")
-  endif()
-endif()
-
-if("Check out an invalid Git repository with error variable specified" MATCHES ${TEST_MATCHES})
   math(EXPR TEST_COUNT "${TEST_COUNT} + 1")
 
   include(GitCheckout)
@@ -54,9 +71,9 @@ if("Check out an invalid Git repository with error variable specified" MATCHES $
     ERROR_VARIABLE ERR
   )
 
-  set(EXPECTED_FATAL_ERROR_MESSAGE "Failed to clone https://github.com/threeal/invalid-project (128)")
-  if(NOT ${ERR} STREQUAL EXPECTED_FATAL_ERROR_MESSAGE)
-    message(FATAL_ERROR "It should fail to check out because of `${EXPECTED_FATAL_ERROR_MESSAGE}`, but got `${ERR}` instead")
+  set(EXPECTED_ERR "Failed to clone https://github.com/threeal/invalid-project (128)")
+  if(NOT ${ERR} STREQUAL EXPECTED_ERR)
+    message(FATAL_ERROR "It should fail to check out because of '${EXPECTED_ERR}' but instead got '${ERR}'")
   endif()
 endif()
 
