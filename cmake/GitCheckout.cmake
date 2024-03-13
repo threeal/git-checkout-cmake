0835e17d65806b8e473d3cf35a09082f4f6de8a5
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-03-13 22:16:23 +0700

    chore: remove unused `_set_error` function

--- cmake/GitCheckout.cmake
+++ cmake/GitCheckout.cmake
@@ -3,23 +3,6 @@
 
 include_guard(GLOBAL)
 
-# Set error with a specified message.
-#
-# If the `ERROR_VARIABLE` is specified, it will set the error message to that variable.
-# Otherwise, it will print the error message and halt the execution.
-#
-# Arguments:
-#   - MESSAGE: The error message.
-macro(_set_error MESSAGE)
-  cmake_parse_arguments(ARG "" "ERROR_VARIABLE" "" ${ARGN})
-  if(ARG_ERROR_VARIABLE)
-    set(${ARG_ERROR_VARIABLE} "${MESSAGE}" PARENT_SCOPE)
-    return()
-  else()
-    message(FATAL_ERROR "${MESSAGE}")
-  endif()
-endmacro()
-
 # Incompletely clones a Git repository from a remote location.
 #
 # It incompletely clones the Git repository to a specified directory without
