428f088da27290c637ce926645fc127e07c378e4
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-04-30 23:20:13 +0700

    test: add test for getting the Git check out directory

--- test/cmake/GitOtherTest.cmake
+++ test/cmake/GitOtherTest.cmake
@@ -12,6 +12,26 @@ function(test_find_the_git_executable)
   endif()
 endfunction()
 
+function(test_get_the_git_check_out_directory_with_a_directory_provided)
+  _get_git_checkout_directory(https://github.com/threeal/project-starter some-directory DIRECTORY)
+
+  if(NOT DEFINED DIRECTORY)
+    message(FATAL_ERROR "The 'DIRECTORY' variable should be defined")
+  elseif(NOT DIRECTORY STREQUAL some-directory)
+    message(FATAL_ERROR "The Git check out directory should be set to 'some-directory' but instead got '${DIRECTORY}'")
+  endif()
+endfunction()
+
+function(test_get_the_git_check_out_directory_without_any_directory_provided)
+  _get_git_checkout_directory(https://github.com/threeal/project-starter "" DIRECTORY)
+
+  if(NOT DEFINED DIRECTORY)
+    message(FATAL_ERROR "The 'DIRECTORY' variable should be defined")
+  elseif(NOT DIRECTORY STREQUAL project-starter)
+    message(FATAL_ERROR "The Git check out directory should be set to 'project-starter' but instead got '${DIRECTORY}'")
+  endif()
+endfunction()
+
 if(NOT DEFINED TEST_COMMAND)
   message(FATAL_ERROR "The 'TEST_COMMAND' variable should be defined")
 elseif(NOT COMMAND test_${TEST_COMMAND})
