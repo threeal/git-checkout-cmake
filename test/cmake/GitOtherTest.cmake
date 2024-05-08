8e6624331baefc16b3cc61b03cd24cecd0f78597
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-05-08 20:46:34 +0700

    test: allow spaces in test function names (#91)
    * test: allow spaces in test function names
    
    * test: remove unnecessary if guard before calling test command

--- test/cmake/GitOtherTest.cmake
+++ test/cmake/GitOtherTest.cmake
@@ -6,38 +6,32 @@ include(${CMAKE_BINARY_DIR}/Assertion.cmake)
 
 include(GitCheckout)
 
-function(test_find_the_git_executable)
+function("Find the Git executable")
   _find_git()
 
   assert_defined(GIT_EXECUTABLE)
   assert_exists("${GIT_EXECUTABLE}")
 endfunction()
 
-function(test_get_the_git_check_out_directory_with_an_absolute_directory_provided)
+function("Get the Git check out directory with an absolute directory provided")
   _get_git_checkout_directory(https://github.com/threeal/project-starter /path/to/some-directory DIRECTORY)
 
   assert_defined(DIRECTORY)
   assert_strequal("${DIRECTORY}" /path/to/some-directory)
 endfunction()
 
-function(test_get_the_git_check_out_directory_with_a_relative_directory_provided)
+function("Get the Git check out directory with a relative directory provided")
   _get_git_checkout_directory(https://github.com/threeal/project-starter some-directory DIRECTORY)
 
   assert_defined(DIRECTORY)
   assert_strequal("${DIRECTORY}" ${CMAKE_CURRENT_BINARY_DIR}/some-directory)
 endfunction()
 
-function(test_get_the_git_check_out_directory_without_any_directory_provided)
+function("Get the Git check out directory without any directory provided")
   _get_git_checkout_directory(https://github.com/threeal/project-starter "" DIRECTORY)
 
   assert_defined(DIRECTORY)
   assert_strequal("${DIRECTORY}" ${CMAKE_CURRENT_BINARY_DIR}/project-starter)
 endfunction()
 
-if(NOT DEFINED TEST_COMMAND)
-  message(FATAL_ERROR "The 'TEST_COMMAND' variable should be defined")
-elseif(NOT COMMAND test_${TEST_COMMAND})
-  message(FATAL_ERROR "Unable to find a command named 'test_${TEST_COMMAND}'")
-endif()
-
-cmake_language(CALL test_${TEST_COMMAND})
+cmake_language(CALL "${TEST_COMMAND}")
